80299b1d7f35a2a400e677ca7cfc1c88f78ece4e
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-17 10:52:55 -0500

    Add login and middleware to project

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const { dbConnection } = require("./db/db");
 const Role = require("./routers/role");
 const User = require("./routers/user");
+const Board = require("./routers/board");
+const Auth = require("./routers/auth");
 require("dotenv").config();
 
 const app = express();
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 app.use("/api/role", Role);
 app.use("/api/user", User);
+app.use("/api/board", Board);
+app.use("/api/auth", Auth);
 
 app.listen(process.env.PORT, () => {
     console.log("Backend running in port ", process.env.PORT);
